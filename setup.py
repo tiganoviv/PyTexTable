f1ee4083d954b06679b68bc35bbf281648827559
Vadim Tiganov <tiganoviv@gmail.com>
2025-05-09 21:46:08 +0300

    Fix package name and update long description handling in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+"""SETUPTOOLS CONFIGURATION FILE """
+# This file is used to configure the setuptools package for the pytextable project.
 from setuptools import setup, find_packages
 
+# Read the long description from README.md using a 'with' statement
+with open("README.md", encoding="UTF-8") as readme_file:
+    long_description = readme_file.read()
+
 setup(
-    name="pylatextable",
+    name="pytextable",
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -11,9 +17,9 @@ setup(
     author="Vadim Tiganov",
     author_email="tiganoviv@gmail.com",
     description="A simple Python tool to generate LaTeX tables",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tiganoviv/pylatextable",
+    url="https://github.com/tiganoviv/pytextable",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
